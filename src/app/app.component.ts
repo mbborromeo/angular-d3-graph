bcff7c05529db5f9cfe0e2449649e42e0a1a056d
mbborromeo <mbborromeo@gmail.com>
2020-05-08 20:09:57 -0700

    add condition to check for empty string for provinceID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import * as d3 from 'd3';
 })
 export class AppComponent {
   data: Promise<any>; // Array<any>, Observable<DataModel>
-  provinceID: number;
+  provinceID: string; // number
   csvUrl: string = 'https://health-infobase.canada.ca/src/data/covidLive/covid19.csv';  
   csvUrlWithProxy: string = `https://cors-anywhere.herokuapp.com/${ this.csvUrl }`;
   localCsv: string = './assets/covid19.csv';
@@ -29,25 +29,27 @@ export class AppComponent {
 
     // using cors-anywhere as a proxy to access the external CSV file
     // d3.csv returns a promise, so needs a return statement inside
-    this.data = d3.csv( this.csvUrlWithProxy ) // this.localCsv
+    if( this.provinceID!=='' ){
+      this.data = d3.csv( this.csvUrlWithProxy ) // this.localCsv
       .then( function (data){
         console.log('AppComponent covid CSV data is', data);  
         console.log('AppComponent TYPE OF CSV DATA', typeof(data) )
                 
         // show data of selected province
         console.log('AppComponent csv() self.provinceID', self.provinceID)
-        const objs = data.filter( d => parseInt(d.pruid)==self.provinceID );        
+        const objs = data.filter( d => d.pruid==self.provinceID ); // parseInt(d.pruid)   
         return objs;        
       })
       .finally( function (){
         console.log('AppComponent csv() self.data!!!!', self.data)
         self.dataLoading = false;
       });
+    }    
   }
 
   /* receive Event Emitter from province-select.component.ts: onChange/this.selected.emit() */
   onSelect( id: string ) {
-    this.provinceID = parseInt( id );
+    this.provinceID = id; // parseInt( id )
     console.log('AppComponent onSelect provinceID', this.provinceID)   
 
     this.getData();
